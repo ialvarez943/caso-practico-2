6ef080835893618f8dac9e72393cf1a970235792
ialvarez <ignacioalvarez2854@gmail.com>
2026-03-18 10:11:14 +0100

    Fix error RequestDisallowedByAzure on regions allowed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "resource_group_name" {
 }
 
 variable "location" {
-  default     = "West Europe"
+  default     = "francecentral"
   description = "Azure region where resources will be deployed"
   type        = string
 }
