fa9fcb4880b03ce5056671983d660dfd6bc5fc44
ialvarez <ignacioalvarez2854@gmail.com>
2026-03-18 10:04:44 +0100

    Added address_space to vn

--- terraform/network.tf
+++ terraform/network.tf
@@ -2,6 +2,7 @@ resource "azurerm_virtual_network" "vn" {
   name                = var.virtual_network_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.10.0.0/16"]
   tags                = var.tags
 }
 
