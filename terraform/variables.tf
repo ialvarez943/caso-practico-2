0bc4ac6829989fdc7ca990199a284a409cb00b5c
ialvarez <ignacioalvarez2854@gmail.com>
2026-03-18 11:53:27 +0100

    Changed vm size value and region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "resource_group_name" {
 }
 
 variable "location" {
-  default     = "spaincentral"
+  default     = "Norway East"
   description = "Azure region where resources will be deployed"
   type        = string
 }
